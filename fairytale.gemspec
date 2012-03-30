e7544bbb9390b178d290c57504e8222a494ff63e
Michael Mokrysz <hello@46bit.com>
2012-03-30 02:43:21 +0100

    Added current gem.

---
+++ fairytale.gemspec
@@ -0,0 +1,11 @@
+Gem::Specification.new do |s|
+  s.name        = 'fairytale'
+  s.version     = '0.7.9.pre'
+  s.date        = '2012-03-30'
+  s.summary     = "Sinatra-like static site compiler."
+  s.description = "Sinatra is awesome for building sites. Fairytale brings it's power and freedom to static site compilation."
+  s.authors     = ["Michael Mokrysz"]
+  s.email       = "hi@46bit.com"
+  s.files       = ["lib/fairytale.rb"]
+  s.homepage    = "http://rubygems.org/gems/fairytale"
+end
---
+++ lib/fairytale.rb
@@ -0,0 +1,57 @@
+require 'rubygems'
+require 'stringio'
+require 'fileutils'
+require 'erb'
+require 'tilt'
+
+module Fairytale
+  def get url, layout = :"views/layout.erb", &block
+    print "Building #{url} ... "
+    view_content = block.call
+    view_content = tilt(layout, url: url) { view_content } unless layout.nil?
+    
+    save url, view_content
+    print "saved"
+    
+    reset!
+    puts
+  end
+  
+  def save url, content
+    path = "webroot#{url}"
+    path += "index.html" if url[-1] == '/'
+    FileUtils.mkdir_p File.dirname path
+    File.open(path, "w+") { |f| f.write content }
+  end
+  
+  def reset!
+    instance_variables.each { |var_symbol| instance_variable_set var_symbol, nil }
+  end
+  
+  def md file, params = {}, &block
+    file = "content/#{file.to_s}" if file.class == Symbol
+    tilt(file, params, &block)
+  end
+  
+  def erb file, params = {}, &block
+    file = "views/#{file.to_s}.erb" if file.class == Symbol
+    tilt(file, params, &block)
+  end
+  
+  def sass file, params = {}, &block
+    file = "webroot/assets/css/#{file.to_s}.sass" if file.class == Symbol
+    tilt(file, params, &block)
+  end
+  
+  def scss file, params = {}, &block
+    file = "webroot/assets/css/#{file.to_s}.scss" if file.class == Symbol
+    tilt(file, params, &block)
+  end
+  
+  def tilt file, params = {}, &block
+    template = Tilt.new file.to_s
+    template.render(self, params) { block.call }
+  end
+end
+
+include Fairytale
